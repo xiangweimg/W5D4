6040dac6c2d915c2ee0eeaef8e7ad442ece2da66
Xiangwei Liu <xiangweimg@gmail.com>
2022-08-18 14:32:18 -0400

    models created

--- practice-for-ch-sql-associations-long-practice/app/models/enrollment.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/enrollment.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
 end
