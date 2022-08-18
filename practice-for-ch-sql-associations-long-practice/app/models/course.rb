ebd432da09de4d4bd0926ce77d3aa8b3af13d764
Xiangwei Liu <xiangweimg@gmail.com>
2022-08-18 15:45:04 -0400

    prereq

--- practice-for-ch-sql-associations-long-practice/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/course.rb
@@ -10,8 +10,25 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
-    has_many :enrollees,
+    has_many :enrollments,
         primary_key: :id,
         foreign_key: :course_id,
         class_name: :Enrollment
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+    
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional: true
+#if self-referring, only belongs to
+
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User 
+
 end
